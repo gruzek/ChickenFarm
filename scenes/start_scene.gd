8765f1fdca9a65d0115e495a113711bc460a1c44
Geo <gruzek@gmail.com>
2025-07-25 12:42:53 -0400

    Basic stuff

---
+++ scenes/start_scene.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+func _ready():
+	# Connect the button's pressed signal to the _on_start_button_pressed function
+	$CanvasLayer/CenterContainer/StartButton.pressed.connect(_on_start_button_pressed)
+
+func _on_start_button_pressed():
+	# Load the main scene when the button is pressed
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
