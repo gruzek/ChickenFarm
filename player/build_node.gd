7e3ea4a57041ccc0fe600877067a235b7eaf77c7
Geo <gruzek@gmail.com>
2025-07-26 12:03:23 -0400

    Build stuff

--- player/build_node.gd
+++ player/build_node.gd
@@ -6,26 +6,44 @@ signal build_mode_exited
 
 var preview_instance: Node3D = null
 var in_build_mode = false
-@export var egg_dispenser_scene: PackedScene
+var current_item_index = 0
+var buildable_scenes: Array[PackedScene]
 @onready var camera_3d: Camera3D = $"../Camera3D"
+
+func _ready():
+	# Load buildable scenes programmatically
+	buildable_scenes = [
+		load("res://models/chicken_coop/chicken_coop.tscn"),
+		load("res://models/egg_dispenser/egg_dispenser.tscn")
+	]
+	print("Build_node: Loaded ", buildable_scenes.size(), " buildable scenes")
 
 func _process(delta: float) -> void:
 
 	# Start build mode
 	if !in_build_mode and Input.is_action_just_pressed("build_thing"):
 		print("Build_node: Entering build mode")
-		in_build_mode = true
-		build_mode_entered.emit()
-		preview_instance = egg_dispenser_scene.instantiate()
-		get_tree().current_scene.add_child(preview_instance)
-		# Make preview semi-transparent
-		preview_instance.set_preview_mode(true)
+		if buildable_scenes.size() > 0:
+			in_build_mode = true
+			build_mode_entered.emit()
+			preview_instance = buildable_scenes[current_item_index].instantiate()
+			get_tree().current_scene.add_child(preview_instance)
+			# Make preview semi-transparent
+			preview_instance.set_preview_mode(true)
 
 	if in_build_mode:
 		var mouse_ground_position = get_mouse_ground_position()
 		if mouse_ground_position:
 			preview_instance.global_position = mouse_ground_position
 		#print(mouse_ground_position)
+		
+		# Item selection
+		if Input.is_action_just_pressed("next_build_item"):
+			print("DEBUG: next_item pressed")
+			switch_to_next_item()
+		if Input.is_action_just_pressed("previous_build_item"):
+			print("DEBUG: previous_item pressed")
+			switch_to_previous_item()
 		
 		# Cancle build
 		if Input.is_action_just_pressed("cancel_build"):
@@ -41,6 +59,32 @@ func _process(delta: float) -> void:
 			preview_instance.set_preview_mode(false)
 			in_build_mode = false
 			build_mode_exited.emit()
+
+# Item switching functions
+func switch_to_next_item():
+	if buildable_scenes.size() > 1:
+		current_item_index = (current_item_index + 1) % buildable_scenes.size()
+		update_preview_item()
+		print("Build_node: Switched to item ", current_item_index)
+
+func switch_to_previous_item():
+	if buildable_scenes.size() > 1:
+		current_item_index = (current_item_index - 1 + buildable_scenes.size()) % buildable_scenes.size()
+		update_preview_item()
+		print("Build_node: Switched to item ", current_item_index)
+
+func update_preview_item():
+	if preview_instance:
+		# Store current position
+		var current_position = preview_instance.global_position
+		# Remove old preview
+		preview_instance.queue_free()
+		# Create new preview
+		preview_instance = buildable_scenes[current_item_index].instantiate()
+		get_tree().current_scene.add_child(preview_instance)
+		# Set preview mode and restore position
+		preview_instance.set_preview_mode(true)
+		preview_instance.global_position = current_position
 
 # For building
 func get_mouse_ground_position():
