b7b62c12ddc781074b8013aac37f4e08fd7d94d8
xruzek <rexruzek@gmail.com>
2025-07-27 13:41:45 -0400

    Added sparks to bullets hitting enemies

--- Gun/bullet.gd
+++ Gun/bullet.gd
@@ -1,5 +1,7 @@
 extends Area3D
 
+@export var sparks_scene: PackedScene
+
 var direction = Vector3.FORWARD
 
 @export var speed = 55
@@ -14,4 +16,7 @@ func _on_timer_timeout() -> void:
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("enemy"):
 		body.health -= damage
+		var sparks = sparks_scene.instantiate()
+		get_tree().current_scene.add_child(sparks)
+		sparks.global_position = global_position
 		queue_free()
